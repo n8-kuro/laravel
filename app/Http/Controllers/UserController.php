5aa2dfcb3bee0f49cf5d6f767ee47b1affa3bbcb
Sashom <sashom@gmail.com>
2026-03-20 10:58:53 +0600

    complete task 7-9(UserController three methods and three views)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,14 @@
 namespace App\Http\Controllers;
 
 class UserController extends Controller {
-    public function show() {
-        return view('post.user',[
-            'userus' => 'Дементрий Титус',
-        ]);
+    public function show1() {
+        return view('post.user_1', ['title' => 'User_1', 'text' => 'Good', 'user' => 'Billy Bob']); 
+    }
+    public function show2() {
+        return view('post.user_2', ['title' => 'User_2', 'text' => 'Old', 'user' => 'McCarty Joe']); 
+
+    }
+    public function show3() {
+        return view('post.user_3', ['title' => 'User_3', 'text' => 'Legend', 'user' => 'Joel Miller']); 
     }
 }
---
+++ resources/views/post/user_1.blade.php
@@ -0,0 +1,11 @@
+<x-layout>
+    <x-slot:title>
+        {{$title}}
+    </x-slot:>
+    <b>
+        {{$text}}
+    </b>
+    <b>
+        {{$user}}
+    </b>
+</x-layout>
