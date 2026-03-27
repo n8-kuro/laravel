7a9a8b982038927be9a905ec60ab938ba7b7c196
qwe <montorik64@gmail.com>
2026-03-27 16:20:40 +0600

    task-6 complete(without date-format, just simple text output

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,6 @@
 namespace App\Http\Controllers;
 class PostController extends Controller {
     public function show() {
-        return view('post.task_5', ['title'=>'number-5', 'text'=>'GitHub', 'href'=>'https://github.com/n8-kuro/laravel']);
+    return view('post.task_6', ['title'=>'number-6', 'date'=>'27.03.2026',]);
     }   
 }
