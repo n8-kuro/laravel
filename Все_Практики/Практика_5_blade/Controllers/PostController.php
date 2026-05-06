edd27518564d6f1f5a0d29f4a7bce7463e553764
n8-kuro <vkassandrarvv13@gmail.com>
2026-05-06 10:24:06 +0600

    complete pr5

---
+++ Все_Практики/Практика_5_blade/Controllers/PostController.php
@@ -0,0 +1,228 @@
+<?php
+namespace App\Http\Controllers;
+class PostController extends Controller {
+    public function show() {
+    $array = [
+        'el1' => 1,
+        'el2' => 2, 
+        'el3' => 3,
+        'el4' => 4,
+        'el5' => 5,
+    ];
+    return view('post.task_8', [
+        'title' => 'number-8', 
+        'array' => $array
+    ]);
+    }
+
+    public function city() {
+        return view('post.task_9', ['title' => 'number-9', 'city' => 'Moscow']);
+    }
+
+    public function task_10() {
+        $array = [
+            'Russia' => 'Moscow',
+            'Finland' => 'Helsinki',
+            'France' => 'Paris',
+            'Spain' => 'Madrid',
+            'Great Britain' => 'London',
+
+        ];
+        return view('post.task_10', ['title' => 'number-10', 'location' => $array]);
+    }
+
+    public function task_11() {
+        return view('post.task_11', ['title'=>'number-11', 'year'=>'2026-', 'month'=>'04-', 'day'=>'03']);
+    }
+
+    public function task_12() {
+        return view('post.task_12', ['title'=>'number-12', 'str'=>'<b>text</b>']);
+    }
+
+    public function task_14() {
+        return view('post.task_14_15_16', ['title'=>'number-14-15-16', 'age'=> 19]);
+    }
+
+    public function task_17() {
+        return view('post.task_17', ['title'=>'number-17', 'age'=>18]);
+    }
+
+    public function task_18() {
+        $array2 = [
+        'n1' => 9,
+        'n2' => 11, 
+        'n3' => 10,
+        'n4' => 40,
+        'n5' => 30,
+    ];
+    return view('post.task_18', [
+        'title' => 'number-18', 
+        'array' => $array2
+    ]);
+    }
+
+    public function task_19() {
+        $array3 = [
+            'q1' => 1,
+            'q2' => 1,
+            'q3' => 2,
+            'q4' => 3,
+            'q5' => 5,
+            'q6' => 8,
+            'q7' => 13,
+        ];
+    return view('post.task_19-21', [
+        'title' => 'number-19-21',
+        'array' => $array3
+    ]);
+    }
+    
+    public function task_22(){
+        $str = [
+            'Num-5' => 'Final Fantasy 13',
+            'Num-4' => 'Disco Elysium',
+            'Num-3' => 'Life is Strange',
+            'Num-2' => 'Trails in the Sky FC',
+            'Num-1' => 'Clair Obscur: Expedition 33',
+        ];
+
+        return view('post.task_22', [
+            'title' => 'number-22',
+            'strings' => $str
+        ]);
+    }
+
+    public function task_24() {
+        $array4 = [
+            'z1' => 1,
+            'z2' => 2,
+            'z3' => 3,
+            'z4' => 5,
+            'z5' => 8,
+            'z6' => 13,
+            'z7' => 21,
+            'z8' => 34,
+            'z9' => 55,
+            'z10' => 89,
+        ];
+
+        return view('post.task_24', [
+            'title' => 'number-24',
+            'array' => $array4,
+        ]);
+    }
+
+    public function task_25() {
+        $data1 = [
+            's1' => 1,
+            's2' => 2,
+            's3' => 3,
+            's4' => 5,
+            's5' => 8,
+            's6' => 13,
+            's7' => 21,
+            's8' => 34,
+            's9' => 55,
+            's10' => 89,
+        ];
+        return view('post.task_25', ['title'=>'number-25', 'data'=>$data1 ]);
+    }
+
+    public function task_26(){
+        $array5 = [
+            [ 
+                'x' => 2,
+                'x2' => 6,
+                'x3' => 9,
+                'x4' => 11,
+                'x5' => 5,
+            ],
+            [
+                'y' => 4,
+                'y2' => 36,
+                'y3' => 81,
+                'y4' => 121,
+                'y5' => 25,
+            ]
+      
+        ];
+        return view('post.task_26', ['title'=>'number-26', 'array'=>$array5]);
+    }
+
+    public function task_27(){
+        $employees= [
+            [
+                'name'=>'user1',
+                'surname'=>'surname1',
+                'salary'=> 1000,
+            ],
+            [
+                'name'=>'user2',
+                'surname'=>'surname2',
+                'salary'=> 2000,
+            ],
+            [
+                'name'=>'user3',
+                'surname'=>'surname3',
+                'salary'=> 3000,
+            ],
+        ];
+        return view('post.task_27', ['title'=>'number-27', 'employe'=>$employees]);
+    }
+
+    public function task_28(){
+        $employees= [
+            [
+                'name'=>'user1',
+                'surname'=>'surname1',
+                'salary'=> 1000,
+            ],
+            [
+                'name'=>'user2',
+                'surname'=>'surname2',
+                'salary'=> 2000,
+            ],
+            [
+                'name'=>'user3',
+                'surname'=>'surname3',
+                'salary'=> 3000,
+            ],
+        ];
+        return view('post.task_28', ['title'=>'number-28', 'employe'=>$employees]);
+    }
+
+    public function task_29() {
+        $users = [
+            [
+                'name'=>'Twillight',
+                'surname'=>'Sparkle',
+            ],
+            [
+                'name'=>'Kanye',
+                'surname'=>'West',
+            ],
+            [
+                'name'=>'Anakin',
+                'surname'=>'Skywalker',
+            ],
+        ];
+        return view('post.task_29', ['title' => 'number-29', 'user'=>$users ]);
+    }
+
+    public function task_30() {
+          $stringus = [
+            [
+                1 => 'Makoto Yuki'
+
+            ],
+            [
+                2 => 'Yu Narukami'
+                
+            ],
+            [
+                3 => 'Ren Amamiya'
+            ]   
+        ];
+        return view('post.task_30', ['title'=>'number-30', 'string'=>$stringus]);
+    }
+}
---
+++ Все_Практики/Практика_5_blade/web.php
@@ -0,0 +1,37 @@
+<?php
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+
+Route::get('/num8', [PostController::class, 'show']);
+
+Route::get('/num9', [PostController::class, 'city']);
+
+Route::get('/num10', [PostController::class, 'task_10']);
+
+Route::get('/num11', [PostController::class, 'task_11']);
+
+Route::get('/num12', [PostController::class, 'task_12']);
+
+Route::get('/num14', [PostController::class, 'task_14']);
+
+Route::get('/num17', [PostController::class, 'task_17']);
+
+Route::get('/num18', [PostController::class, 'task_18']);
+
+Route::get('/num19', [Postcontroller::class, 'task_19']);
+
+Route::get('/num22', [PostController::class, 'task_22']);
+
+Route::get('/num24', [PostController::class, 'task_24']);
+
+Route::get('/num25', [PostController::class, 'task_25']);
+
+Route::get('/num26', [PostController::class, 'task_26']);
+
+Route::get('/num27', [PostController::class, 'task_27']);
+
+Route::get('/num28', [PostController::class, 'task_28']);
+
+Route::get('/num29', [PostController::class, 'task_29']);
+
+Route::get('/num30', [PostController::class, 'task_30']);
