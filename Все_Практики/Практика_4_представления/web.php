04af8c38f0a5556da5b38b5a9ad9f4dc7d6dbcec
n8-kuro <vkassandrarvv13@gmail.com>
2026-05-06 11:33:08 +0600

    nc

--- Все_Практики/Практика_4_представления/web.php
+++ Все_Практики/Практика_4_представления/web.php
@@ -1,4 +1,4 @@
-// <?php
+<!-- // ?php
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
@@ -8,4 +8,4 @@ use App\Http\Controllers\UserController;
 // Задание 7-9
 // Route::get('/person1', [UserController::class, 'show1']);
 // Route::get('/person2', [UserController::class, 'show2']);
-// Route::get('/person3', [UserController::class, 'show3']);
+// Route::get('/person3', [UserController::class, 'show3']); -->
